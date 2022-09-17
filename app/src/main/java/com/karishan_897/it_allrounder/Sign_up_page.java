1438e425de4f3812328f81ba454aa041d87c129e
karishan <karishan.897@gmail.com>
2022-09-17 13:51:57 +0500

    .

---
+++ app/src/main/java/com/karishan_897/it_allrounder/Sign_up_page.java
@@ -0,0 +1,14 @@
+package com.karishan_897.it_allrounder;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Sign_up_page extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up_page);
+    }
+}
